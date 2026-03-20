61a2ce9cbac4e9767dd5150714c341c4f957d40b
Xavier Hampton <xaviermichaelhampton@gmail.com>
2026-03-20 00:04:42 -0500

    added optional ssl handling in database
    Easier for testing, with local database we can't use SSL, so there is
    now an option to disable it

--- server/db.js
+++ server/db.js
@@ -2,16 +2,18 @@ require('dotenv').config();
 const { Pool } = require('pg');
 const fs = require('fs');
 
+const sslConfig = process.env.DB_SSL === 'false' ? false : {
+  rejectUnauthorized: false,
+  ca: fs.readFileSync('./certs/global-bundle.pem').toString(),
+};
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: 5432,
   database: 'postgres',
   user: 'postgres',
   password: process.env.DB_PASSWORD,
-  ssl: {
-    rejectUnauthorized: false,
-    ca: fs.readFileSync('./certs/global-bundle.pem').toString(),
-  },
+  ssl: sslConfig,
   min: 1,
 });
 
