9d6f947587af092e264507c37ade53a30283f4ed
keehin <ktsmccann@gmail.com>
2026-04-13 20:24:21 -0500

    please don't push this

---
+++ mobile/DatasetBuilderApp/app/config.js
@@ -0,0 +1,4 @@
+// Change this to your EC2 server IP/hostname
+// For Android emulator local testing: 'http://10.0.2.2:3000'
+export const API_BASE_URL =
+  "http://ec2-3-144-42-190.us-east-2.compute.amazonaws.com";
