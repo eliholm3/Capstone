76a1f90c9d2c8b85dcd3a7026ec62f25b78df420
keehin <ktsmccann@gmail.com>
2026-04-13 20:31:37 -0500

    pushing secrets

--- mobile/DatasetBuilderApp/app/config.js
+++ mobile/DatasetBuilderApp/app/config.js
@@ -1,4 +1,1 @@
-// Change this to your EC2 server IP/hostname
-// For Android emulator local testing: 'http://10.0.2.2:3000'
-export const API_BASE_URL =
-  "http://ec2-3-144-42-190.us-east-2.compute.amazonaws.com";
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL || "http://localhost:3000";
